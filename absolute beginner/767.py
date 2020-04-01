0982d49c6042cc6c8a4c967461c2945750aab395
mahalakshmi77 <62943138+mahalakshmi77@users.noreply.github.com>
2020-04-01 10:53:08 +0530

    Create 767.py

---
+++ absolute beginner/767.py
@@ -0,0 +1,27 @@
+// You are given three numbers A, B & C. Print the largest amongst these three numbers.
+
+Input Description:
+Three numbers are provided to you.
+
+Output Description:
+Find and print the largest among the three
+
+Sample Input :
+1
+2
+3
+Sample Output :
+3  //
+
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+ 
+if (num1 > num2) and (num1 > num3):
+   largest = num1
+elif (num2 > num1) and (num2 > num3):
+   largest = num2
+else:
+   largest = num3
+ 
+print(largest)
