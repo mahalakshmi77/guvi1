401b6094d698430480db98fbd490c8539c888b52
mahalakshmi77 <62943138+mahalakshmi77@users.noreply.github.com>
2020-04-01 11:05:47 +0530

    Create 779.py

---
+++ absolute beginner/779.py
@@ -0,0 +1,18 @@
+// You are provided with two numbers. Find and print the smaller number.
+
+Input Description:
+You are provided with two numbers as input.
+
+Output Description:
+Print the small number out of the two numbers.
+
+Sample Input :
+23 1
+Sample Output :
+1 //
+
+A,B =map(int, input().split())
+if(A>B):
+ print(B)
+else:
+ print(A)
