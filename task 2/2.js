ddb4eabd18095f19e857859798c6305e3ec59ec8
B.mahalakshmi <62943138+mahalakshmi77@users.noreply.github.com>
2020-04-03 10:49:12 +0530

    Create 2.js

---
+++ task 2/2.js
@@ -0,0 +1,35 @@
+// var num=[1 , 2.4 ,"name" , true, {} ,null , ,[0]]
+console.log(num[0]);
+console.log(typeof num[0]);
+console.log(num[1]);
+console.log(typeof num[1]);
+console.log(num[2]);
+console.log(typeof num[2]);
+console.log(num[3]);
+console.log(typeof num[3]);
+console.log(num[4]);
+console.log(typeof num[4]);
+console.log(num[5]);
+console.log(typeof num[5]);
+console.log(num[6]);
+console.log(typeof num[6]);
+console.log(num[7]);
+console.log(typeof num[7]); //
+
+output:
+>>> 1
+number
+2.4
+number
+name
+string
+true
+boolean
+{}
+object
+null
+object
+undefined
+undefined
+[ 0 ]
+object
