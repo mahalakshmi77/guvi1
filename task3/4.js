60689233a89a02986d67d212bc84fbe99bdc423c
B.mahalakshmi <62943138+mahalakshmi77@users.noreply.github.com>
2020-04-04 10:56:29 +0530

    Create 4.js

---
+++ task3/4.js
@@ -0,0 +1,28 @@
+//function calculator(){
+  var a=3;
+  var b=5;
+  console.log(sum=a+b);
+  console.log(sub=a-b);
+  console.log(mul=a*b);
+  console.log(div=a/b);
+  return
+  }
+calculator()
+c=sum;
+d=sub;
+e=mul;
+f=div;
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f); //
+
+Output:
+>>> 8
+-2
+15
+0.6
+8
+-2
+15
+0.6
