563bf655b95faa40a730b0a85d7968abbf384a4a
B.mahalakshmi <62943138+mahalakshmi77@users.noreply.github.com>
2020-04-28 23:42:43 +0530

    Create name with space.js

---
+++ warmup.js/name with space.js
@@ -0,0 +1,11 @@
+//Name separated by space:
+
+function getFullName(firstName, lastName){
+ var a=firstName + " " +lastName;
+ return a;
+}
+var b=getFullName("GUVI","GEEK");
+console.log(b)
+
+output:
+GUVI GEEK
