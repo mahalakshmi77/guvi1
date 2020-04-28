111565b447eda3c4c4f02577836f42569ce461a4
B.mahalakshmi <62943138+mahalakshmi77@users.noreply.github.com>
2020-04-28 23:41:21 +0530

    Create oddnum.js

---
+++ warmup.js/oddnum.js
@@ -0,0 +1,17 @@
+//	Both are odd:
+
+function areBothOdd(num1, num2){
+ if((num1%2!== 0) && (num2%2!== 0))
+ {
+     console.log("true")
+ }
+ else
+ {
+     console.log("false")
+ }
+}
+areBothOdd(9,19)
+
+
+output:
+true
