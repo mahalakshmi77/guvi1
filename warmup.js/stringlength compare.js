fa151e1de7d55ef1a979efdd4088ee26c88b53ba
B.mahalakshmi <62943138+mahalakshmi77@users.noreply.github.com>
2020-04-28 23:45:28 +0530

    Create stringlength compare.js

---
+++ warmup.js/stringlength compare.js
@@ -0,0 +1,18 @@
+//stringlength comp
+
+function isSameLength(word1, word2){
+ var a=word1.length;
+ var b=word2.length;
+ if(a==b)
+ {
+     console.log("true")
+ }
+ else
+ {
+     console.log("false")
+ }
+}
+isSameLength("GUVI","GEEK")
+
+output:
+true
