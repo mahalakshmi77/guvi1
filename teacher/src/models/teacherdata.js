893a6f19607afb537c984dcb52a7615dbe9260a8
B.mahalakshmi <62943138+mahalakshmi77@users.noreply.github.com>
2020-07-17 13:05:15 +0530

    Create teacherdata.js

---
+++ teacher/src/models/teacherdata.js
@@ -0,0 +1,219 @@
+const teachers = [
+  {
+    id: 1,
+    firstName: "A",
+    lastName: "b",
+    email: "Ab@gmail.com",
+    age: 16,
+    gender: "male",
+    students: [
+      {
+        id: 1,
+        firstName: "c",
+        lastName: "d",
+        age: 16,
+        gender: "male",
+        scores: {
+          math: 90,
+          english: 90,
+          science: 90,
+          tamil: 90
+        }
+      },
+      {
+        id: 2,
+        firstName: "c",
+        lastName: "d",
+        age: 16,
+        gender: "male",
+        scores: {
+          math: 90,
+          english: 90,
+          science: 90,
+          tamil: 90
+        }
+      },
+      {
+        id: 3,
+       firstName: "c",
+        lastName: "d",
+        age: 16,
+        gender: "male",
+        scores: {
+          math: 90,
+          english: 90,
+          science: 90,
+          tamil: 90
+        }
+      },
+      {
+        id: 4,
+firstName: "c",
+        lastName: "d",
+        age: 16,
+        gender: "male",
+        scores: {
+          math: 90,
+          english: 90,
+          science: 90,
+          tamil: 90
+        }
+      },
+      {
+        id: 5,
+       firstName: "c",
+        lastName: "d",
+        age: 16,
+        gender: "male",
+        scores: {
+          math: 90,
+          english: 90,
+          science: 90,
+          tamil: 90
+        }
+      }
+    ]
+  },
+
+  {
+    id: 2,
+    firstName: "e",
+    lastName: "f",
+    email: "ef@gmail.com",
+    age: 16,
+    gender: "male",
+    students: [
+      {
+        id: 1,
+        firstName: "g",
+        lastName: "h",
+        age: 16,
+        gender: "male",
+        scores: {
+          math: 90,
+          english: 90,
+          science: 90,
+          tamil: 90
+        }
+      },
+      {
+        id: 2,
+        firstName: "g",
+        lastName: "h",
+        age: 16,
+        gender: "male",
+        scores: {
+          math: 90,
+          english: 90,
+          science: 90,
+          tamil: 90
+        }
+      },
+      {
+        id: 3,
+ firstName: "g",
+        lastName: "h",
+        age: 16,
+        gender: "female",
+        scores: {
+          math: 90,
+          english: 90,
+          science: 90,
+          tamil: 90
+        }
+      },
+      {
+        id: 4,
+        firstName: "g",
+        lastName: "h",
+        age: 16,
+        gender: "male",
+        scores: {
+          math: 90,
+          english: 90,
+          science: 90,
+          tamil: 90
+        }
+      }
+    ]
+  },
+
+  {
+    id: 3,
+    firstName: "i",
+    lastName: "j",
+    email: "ij@gmail.com",
+    age: 30,
+    gender: "male",
+    students: [
+      {
+        id: 1,
+        firstName: "k",
+lastName: "l",
+age: 16,
+        gender: "male",
+        scores: {
+          math: 90,
+          english: 90,
+          science: 90,
+          tamil: 90
+        }
+      },
+      {
+        id: 2,
+       firstName: "k",
+lastName: "l",
+        age: 16,
+        gender: "male",
+        scores: {
+          math: 90,
+          english: 90,
+          science: 90,
+          tamil: 90
+        }
+      },
+      {
+        id: 3,
+       firstName: "k",
+lastName: "l",
+        age: 16,
+        gender: "male",
+        scores: {
+          math: 90,
+          english: 90,
+          science: 90,
+          tamil: 90
+        }
+      },
+      {
+        id: 4,
+      firstName: "k",
+lastName: "l",
+        age: 16,
+        gender: "male",
+        scores: {
+          math: 90,
+          english: 90,
+          science: 90,
+          tamil: 90
+        }
+      },
+      {
+        id: 5,
+       firstName: "k",
+lastName: "l",
+        age: 16,
+        gender: "female",
+        scores: {
+          math: 90,
+          english: 90,
+          science: 90,
+          tamil: 90
+        }
+      },
+      
+    ]
+  },
+];
+
+module.exports = teachers;
