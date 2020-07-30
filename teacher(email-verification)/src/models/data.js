eea03d6e117fa73c1b4de492d8376af078cbca9b
B.mahalakshmi <62943138+mahalakshmi77@users.noreply.github.com>
2020-07-30 10:44:08 +0530

    Create data.js

---
+++ teacher(email-verification)/src/models/data.js
@@ -0,0 +1,218 @@
+const teachers = [
+  {
+    id: 1,
+    firstName: "A",
+    lastName: "b",
+    email: "Ab@gmail.com",
+    age: 16,
+    gender: "male",
+    students: [
+      {
+        id: 1,
+        firstName: "akash",
+        lastName: "kumar",
+        age: 16,
+        gender: "male",
+        scores: {
+          math: 90,
+          english: 90,
+          science: 90,
+          tamil: 90
+        }
+      },
+      {
+        id: 2,
+        firstName: "c",
+        lastName: "d",
+        age: 16,
+        gender: "male",
+        scores: {
+          math: 90,
+          english: 90,
+          science: 90,
+          tamil: 90
+        }
+      },
+      {
+        id: 3,
+        firstName: "c",
+        lastName: "d",
+        age: 16,
+        gender: "male",
+        scores: {
+          math: 90,
+          english: 90,
+          science: 90,
+          tamil: 90
+        }
+      },
+      {
+        id: 4,
+        firstName: "c",
+        lastName: "d",
+        age: 16,
+        gender: "male",
+        scores: {
+          math: 90,
+          english: 90,
+          science: 90,
+          tamil: 90
+        }
+      },
+      {
+        id: 5,
+        firstName: "c",
+        lastName: "d",
+        age: 16,
+        gender: "male",
+        scores: {
+          math: 90,
+          english: 90,
+          science: 90,
+          tamil: 90
+        }
+      }
+    ]
+  },
+
+  {
+    id: 2,
+    firstName: "C",
+    lastName: "D",
+    email: "CD@gmail.com",
+    age: 16,
+    gender: "male",
+    students: [
+      {
+        id: 1,
+        firstName: "akshaya",
+        lastName: "ravi",
+        age: 16,
+        gender: "male",
+        scores: {
+          math: 90,
+          english: 90,
+          science: 90,
+          tamil: 90
+        }
+      },
+      {
+        id: 2,
+        firstName: "g",
+        lastName: "h",
+        age: 16,
+        gender: "male",
+        scores: {
+          math: 90,
+          english: 90,
+          science: 90,
+          tamil: 90
+        }
+      },
+      {
+        id: 3,
+        firstName: "g",
+        lastName: "h",
+        age: 16,
+        gender: "female",
+        scores: {
+          math: 90,
+          english: 90,
+          science: 90,
+          tamil: 90
+        }
+      },
+      {
+        id: 4,
+        firstName: "g",
+        lastName: "h",
+        age: 16,
+        gender: "male",
+        scores: {
+          math: 90,
+          english: 90,
+          science: 90,
+          tamil: 90
+        }
+      }
+    ]
+  },
+
+  {
+    id: 3,
+    firstName: "E",
+    lastName: "F",
+    email: "EF@gmail.com",
+    age: 30,
+    gender: "male",
+    students: [
+      {
+        id: 1,
+        firstName: "rajesh",
+        lastName: "kumar",
+        age: 16,
+        gender: "male",
+        scores: {
+          math: 90,
+          english: 90,
+          science: 90,
+          tamil: 90
+        }
+      },
+      {
+        id: 2,
+        firstName: "k",
+        lastName: "l",
+        age: 16,
+        gender: "male",
+        scores: {
+          math: 90,
+          english: 90,
+          science: 90,
+          tamil: 90
+        }
+      },
+      {
+        id: 3,
+        firstName: "naveen",
+        lastName: "kumar",
+        age: 16,
+        gender: "male",
+        scores: {
+          math: 90,
+          english: 90,
+          science: 90,
+          tamil: 90
+        }
+      },
+      {
+        id: 4,
+        firstName: "sachin",
+        lastName: "kumar",
+        age: 16,
+        gender: "male",
+        scores: {
+          math: 90,
+          english: 90,
+          science: 90,
+          tamil: 90
+        }
+      },
+      {
+        id: 5,
+        firstName: "k",
+        lastName: "l",
+        age: 16,
+        gender: "female",
+        scores: {
+          math: 90,
+          english: 90,
+          science: 90,
+          tamil: 90
+        }
+      }
+    ]
+  }
+];
+
+module.exports = teachers;
